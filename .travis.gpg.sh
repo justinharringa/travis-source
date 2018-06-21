15453fa968d6f60a1e4fe8296801542cb5a3e98b
Justin Harringa <jharringa@salesforce.com>
2018-06-21 10:34:00 -0700

    Remove double bracket

--- .travis.gpg.sh
+++ .travis.gpg.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Made it to gpg file"
 echo "Can we see GPG_NAME? If so, here it is: ${GPG_NAME}"
-if [[ ! -z "$TRAVIS" && ! -z "$GPG_NAME" ]]; then
+if [ ! -z "$TRAVIS" && ! -z "$GPG_NAME" ]; then
     echo "I see ${TRAVIS} and ${GPG_NAME}!"
 else
     echo "No! I don't see it!"
