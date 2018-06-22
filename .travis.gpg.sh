20deda5d93c1c8569065d45f9e2cded2d0bb5324
Justin Harringa <justin@harringa.com>
2018-06-21 21:53:22 -0700

    Try reversed logic again

--- .travis.gpg.sh
+++ .travis.gpg.sh
@@ -3,9 +3,9 @@ set -e
 
 echo "Made it to gpg file"
 echo "Can we see GPG_NAME? If so, here it is: ${GPG_NAME}"
-if [[ -z "${TRAVIS}" || -z "${GPG_NAME}" ]]; then
+if [[ ! -z "${TRAVIS}" && ! -z "${GPG_NAME}" ]]; then
+    echo "I see ${TRAVIS} and ${GPG_NAME}!"
+else
     echo "No! I don't see it!"
     exit 1
-else
-    echo "I see ${TRAVIS} and ${GPG_NAME}!"
 fi
